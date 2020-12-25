4f036fd39dfc5a7d094fdfdc5930498df453809d
Maggie Yi <myi_gomboc@berkeley.edu>
2020-12-25 00:33:33 -0800

    alternate players

--- src/index.js
+++ src/index.js
@@ -2,21 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    // constructor(props) {
-    //     super(props); //always needed
-    //     this.state = {
-    //         value: null,
-    //     };
-    // }
-    render() {
-        //pass in data, arrow function
-        return (
-            <button className="square" onClick={ () => this.props.onClick() } >
-                {this.props.value}
-            </button>
-        );
-    }
+// class Square extends React.Component {
+//     // constructor(props) {
+//     //     super(props); //always needed
+//     //     this.state = {
+//     //         value: null,
+//     //     };
+//     // }
+//     render() {
+//         //pass in data, arrow function
+//         return (
+//             <button className="square" onClick={ () => this.props.onClick() } >
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+function Square(props) {
+    return (
+        <button className={"square"} onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -25,6 +33,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true, // default: X is first, initially value is null and will change to X the first turn
         };
     }
 
@@ -40,8 +49,11 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     render() {
